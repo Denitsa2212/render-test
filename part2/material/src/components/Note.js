b85a6c8fa40ef5adeffd6f1d08a00b36a7d5db51
Denitsa2212 <denitsamarianova@gmail.com>
2023-07-08 19:52:55 +0300

    d

---
+++ part2/material/src/components/Note.js
@@ -0,0 +1,11 @@
+const Note = ({ note, toggleImportance }) => {
+    const label = note.important ? "Make not important" : "Make important";
+    return (
+      <>
+        <li>{note.content}</li>
+        <button onClick={toggleImportance}>{label}</button>
+      </>
+    )
+}
+
+export default Note
